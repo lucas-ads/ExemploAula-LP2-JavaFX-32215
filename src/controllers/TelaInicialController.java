be7ecc956251bbc7d71e596ea4d77eba33aa2944
1361781 <1361781@laboratorio.ifms.br>
2023-06-19 21:22:02 -0400

    Programação do carregamento dos usuários para a tela inicial

--- src/controllers/TelaInicialController.java
+++ src/controllers/TelaInicialController.java
@@ -2,27 +2,65 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import dao.DaoUsuario;
+import entidades.Usuario;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class TelaInicialController implements Initializable{
 
 	@FXML
 	private BorderPane telaInicial;
+
+	@FXML
+	private TableView<Usuario> tabelaUsuarios;
 	
-	//private Stage stage;
+	@FXML
+	private TableColumn<Usuario, Integer> colunaID;
+	
+	@FXML
+	private TableColumn<Usuario, String> colunaEmail;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		colunaID.setCellValueFactory( new PropertyValueFactory<>("id") );
+		colunaEmail.setCellValueFactory( new PropertyValueFactory<>("email") );
+		
+		DaoUsuario daoUsuario = new DaoUsuario();
+		
+		try {
+			List<Usuario> users = daoUsuario.buscarUsuarios();
+			
+			tabelaUsuarios.setItems( FXCollections.observableArrayList( users ) );
+		
+		} catch (SQLException e) {
+			
+			Alert a = new Alert(AlertType.ERROR);
+			a.setHeaderText("Atenção!");
+			a.setContentText("Erro ao carregar usuários: " + e.getMessage());
+			
+			a.show();
+			
+		}
+		
 		
 	}
-		
+	
 	public void clickCadastrar() throws IOException {
 		Stage stage = (Stage) this.telaInicial.getScene().getWindow();
 		
--- src/module-info.java
+++ src/module-info.java
@@ -4,5 +4,6 @@ module AulaJavaFX {
 	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
-	opens controllers to javafx.fxml;
+	opens controllers to javafx.graphics, javafx.fxml;
+	opens entidades to javafx.base;
 }
